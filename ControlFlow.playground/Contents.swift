dc9f8529623b3c9df7229fc2cd9c1bf527de36ae
Pasan Premaratne <pasan@premaratne.me>
2019-05-20 21:39:47 -0400

    S1V2

--- ControlFlow.playground/Contents.swift
+++ ControlFlow.playground/Contents.swift
@@ -8,7 +8,21 @@ for _ in 1...10 {
     print("Swift")
 }
 
+// While Loops
 
+let isLoggedIn = true
+let isGreater = 10 > 100
+
+//while 10 < 100 {
+//    print(1)
+//}
+
+var counter = 11
+
+while counter < 10 {
+    print(counter)
+    counter += 1
+}
 
 
 
